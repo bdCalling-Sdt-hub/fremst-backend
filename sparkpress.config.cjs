1731e47d02becec4e818c493fa69b7dffda18277
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-11-23 17:35:04 +0600

    changing my config file

---
+++ sparkpress.config.cjs
@@ -0,0 +1,8 @@
+const config = {
+  postman_api_key:
+    'PMAK-6736b96ccbb06e00018c2086-b6e7c760ec61406708332f784ba867dd99',
+  postman_workspace_id: '77518a4f-e105-406e-9bab-547413acecd0',
+  postman_collection_name: 'FREMST-BACKEND',
+};
+
+module.exports = { config };
